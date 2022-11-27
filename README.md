a158308732df9fda02401f83500875dffcefa014
ezmaa <ezinmark@gmail.com>
2022-11-26 23:20:36 -0500

    changed questioons array

---
+++ README.md
@@ -0,0 +1,21 @@
+# README Generator
+https://github.com/Ezmaa/README Generator
+# Description
+idk
+# Table of Contents
+* [Installatioo]{#installation}
+* [Usage]{#usage}
+* [License]{#license}
+* [Contributing]{#contributing}
+* [Tests]{#tests}
+* [Questions]{#questions}
+# Installation 
+inquirer is a required dependenci in order to run the application.
+# Usage
+In order to run the application, node index.js
+# License 
+This README.md generator is licensed under the MIT license.
+# Contributing 
+Contributers: me
+# Contact Me:
+Email: ezinmark@gmail.com
--- index.js
+++ index.js
@@ -3,64 +3,62 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 
-const generateMarkdown = require('./utils/generateMarkdown');
+const generatorMarkdown = require('./utils/generateMarkdown');
 
 
 
 // TODO: Create an array of questions for user input
-const questions = () => {
-    return inquirer.prompt([
-        {
-            type: "input",
-            message: "What is the title of your project?",
-            name: "title",
-        }, 
-        {
-            type: "input",
-            message: "What is the description of your project?",
-            name: "description",
-        }, 
-        {
-            type: "input",
-            message: "What is your github username?",
-            name: "name",
-        }, 
-        {
-            type: "input",
-            message: "What does the user need to install for this application?",
-            name: "installation",
-        }, 
-        {
-            type: "input",
-            message: "What command is used to run this application?",
-            name: "command",
-        }, 
-        {
-            type: "input",
-            message: "What is your email address?",
-            name: "email",
-        },
-        {
-            type: "input",
-            message: "Who contributed to the project?",
-            name: "contributions",
-        },
-        {
-            type: "input",
-            message: "What is the license being used?",
-            name: "license",
-        }, 
-        {
-            type: "input",
-            message: "Table of Contents",
-            name: "table of contents",
-        },
-    ]);
-};
+const questions = [{
+
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title"
+},
+{
+    type: "input",
+    message: "What is the description of your project?",
+    name: "description"
+},
+{
+    type: "input",
+    message: "What is your github username?",
+    name: "name"
+},
+{
+    type: "input",
+    message: "What does the user need to install for this application?",
+    name: "installation"
+},
+{
+    type: "input",
+    message: "What command is used to run this application?",
+    name: "command"
+},
+{
+    type: "input",
+    message: "What is your email address?",
+    name: "email"
+},
+{
+    type: "input",
+    message: "Who contributed to the project?",
+    name: "contributions"
+},
+{
+    type: "input",
+    message: "What is the license being used?",
+    name: "license"
+},
+{
+    type: "input",
+    message: "Table of Contents",
+    name: "table of contents"
+}];
+   
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, function(err) {
+    fs.writeFile(fileName, data, function (err) {
         console.log(fileName)
         console.log(data)
         if (err) {
@@ -69,16 +67,16 @@ function writeToFile(fileName, data) {
             console.log("README Successfully Generated")
         }
     })
- }
+}
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
-    .then(function(data) {
-        writeToFile("README.md", generateMarkdown(data));
-        console.log(data)
-    })
- }
+        .then(function (data) {
+            writeToFile("README.md", generatorMarkdown(data));
+            console.log(data)
+        })
+}
 
 // Function call to initialize app
 init();
